c472bc873cb3525a83679a0190c0d05d4700b204
JaeWonLee <leejaewon264@gmail.com>
2024-11-27 18:19:40 +0900

    1

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+# 베이스 이미지
+FROM openjdk:17-jdk-alipine
+
+WORKDIR /app
+
+# JAR 파일 경로 설정
+ARG JAR_FILE=build/libs/PostDataToWeb-0.0.1-SNAPSHOT.jar
+
+# JAR 파일을 컨테이너로 복사
+COPY ${JAR_FILE} app.jar
+
+EXPOSE 2345
+
+# 컨테이너에서 애플리케이션 실행
+ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/com/gmail/leejaewon264/postdatatoweb/controller/UserInfoController.java
+++ src/main/java/com/gmail/leejaewon264/postdatatoweb/controller/UserInfoController.java
@@ -5,9 +5,11 @@ import com.gmail.leejaewon264.postdatatoweb.repository.UserInfoRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @Controller
 public class UserInfoController {
     private final UserInfoRepository userInfoRepository;
